cf039d0944f6bc2f385308e89a0973d4214f6c4d
laurayess <56653712+laurayess@users.noreply.github.com>
2022-11-28 17:49:19 -0600

    Se agrego la configuracion de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    },
+    manifest: {
+      name: 'PWA Votación de Nuevo León',
+      short_name: 'PWA Votación NL',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png', // <== don't add slash, for testing
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png', // <== don't remove slash, for testing
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable.png', // <== don't add slash, for testing
+          sizes: '600x600',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  })]
 })
