3c9d4c7007166ff6eee2044c1008cd977a84f655
kevin chavez <18161097@itoaxaca.edu.mx>
2022-11-30 00:22:40 -0600

    Agregando el entorno de homepreparacion

---
+++ src/module-preparacion/pages/HomePreparacion.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HomePreparacion = () => {
+  return <div>HOME PREPARACIÓN</div>;
+};
--- src/module-preparacion/routes/PreparacionRoutes.jsx
+++ src/module-preparacion/routes/PreparacionRoutes.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { PrivateRoute } from "../../router/PrivateRoute";
 import { InicioPage } from "../pages/InicioPage";
+import { HomePreparacion } from "../pages/HomePreparacion";
 
 export const PreparacionRoutes = () => {
   return (
     <PrivateRoute>
       <Routes>
         <Route path="inicio" element={<InicioPage></InicioPage>} />
+        <Route path="homePreparacion" element={<HomePreparacion></HomePreparacion>} />
 
+        
+        <Route path="/*" element={<Navigate to="/preparacion/homePreparacion" />} />
         <Route path="/*" element={<Navigate to="/preparacion/inicio" />} />
+        
+        
       </Routes>
     </PrivateRoute>
   );
