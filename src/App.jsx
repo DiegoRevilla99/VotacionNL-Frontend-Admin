c6ccad26d2635730534ee9bab0751f5c6be8e953
Diego Revilla <string.diego.revilla@gmail.com>
2022-11-29 20:34:03 -0600

    Añadiendo estructura de carpetas y sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./styles/generalContainer.css";
+import { SidebarCustom } from "./ui/components/SidebarCustom";
+import { Topbar } from "./ui/components/Topbar";
 
 export const App = () => {
-	return <div className="generalContainer">HOLA MUNDO</div>;
+	return (
+		<div className="app">
+			<SidebarCustom />
+			<main className="content">
+				<Topbar />
+			</main>
+			{/* <div className="generalContainer">HOLA MUNDO</div> */}
+		</div>
+	);
 };
 
 export default App;
---
+++ src/ui/components/Topbar.jsx
@@ -0,0 +1,11 @@
+import { Box, IconButton, Typography } from "@mui/material";
+
+export const Topbar = () => {
+	return (
+		<Box display="flex" justifyContent="center" p={2}>
+			<Typography variant="h4" align="center" color="initial">
+				MODULO DE PREPARACIÓN
+			</Typography>
+		</Box>
+	);
+};
