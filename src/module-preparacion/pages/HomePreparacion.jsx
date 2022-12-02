82dd291624929ffa419d34421dc6b8f9e1cf89c4
kevin chavez <18161097@itoaxaca.edu.mx>
2022-12-02 16:05:02 -0600

    Agregué las ediciones de los botones y acomodé el cuadro para que sea mejor, sigo trabajando en los botones porque no me sale el borde y el sombreado

--- src/module-preparacion/pages/HomePreparacion.jsx
+++ src/module-preparacion/pages/HomePreparacion.jsx
@@ -1,40 +1,66 @@
 import React from "react";
-import { Button, Paper } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+const useStyle = makeStyles({
+  boton: {
+    background: 'linear-gradient(45deg, #511079 30%, #511079 90%)',
+    borderRadius: '1px 19px 19px',
+    border: 1,
+    boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .23)",
+    color: "white",
+    padding: "0 30px",
+    width: 230,
+    height: 270,
+    '&:hover': {
+      background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+  },
+  },
+});
 
 export const HomePreparacion = () => {
+  const classes = useStyle();
   return (
     <>
     <div>
-        <Paper>
-          <Box p={1}>
-            <h2>PREPARACIÓN</h2>
-            </Box>
-        </Paper>
+      <Box ml={3}>
+      <h3>PREPARACIÓN</h3></Box>
+      <hr/>
     </div>
-    <div>
-    <Box p={8} sx={{
-        backgroundColor: '#FFFFFF',
-        borderRadius: 10,
-        border: 1,
-        boxShadow: 5,
-        display: 'grid',
-        alignItems: 'center',
-        width: 900,
-        height: 100,
-        }}>
-      <h3> ¿Qué tipo de plantilla desea usar?</h3>
-      <Stack direction="row"
-            justifyContent="center"
-            alignItems="center"
-            spacing={14}
-            >  
-        <Button variant="contained" href="HomeJElectoral">PLANTILLA DE LA JORNADA ELECTORAL</Button>
-        <Button variant="contained">PLANTILLA DEL COMITÉ</Button>
-        <Button variant="contained">PLANTILLA DE LA CONSULTA CIUDADANA</Button>
-      </Stack>
-    </Box>
+
+    <div align = "center">
+
+      <Box mt={5}>
+        <Box p={8} sx={{
+          backgroundColor: '#FFFFFF',
+          borderRadius: 10,
+          border: 1,
+          boxShadow: 5,
+          width: 900,
+          height: 320,
+          }}
+          
+          >
+            <Typography variant="h5" align="left" color="initial" paragraph>
+              ¿Qué tipo de plantilla desea usar?
+            </Typography>
+        
+        <Stack direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={10}
+              
+              >  
+          <Button variant="contained" href="HomeJElectoral" className={classes.boton} >PLANTILLA DE LA JORNADA ELECTORAL</Button>
+          <Button variant="contained" className={classes.boton}>PLANTILLA DEL COMITÉ</Button>
+          <Button variant="contained" className={classes.boton}>PLANTILLA DE LA CONSULTA CIUDADANA</Button>
+        </Stack>
+      </Box>
+      </Box>
 
     </div>
     
