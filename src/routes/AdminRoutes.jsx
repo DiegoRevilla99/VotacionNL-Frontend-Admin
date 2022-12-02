d9ce7afa8b8e76ea39ffa1988a65599bb3c2ee81
Diego Revilla <string.diego.revilla@gmail.com>
2022-12-01 22:30:38 -0600

    Mergeando con rutas

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -7,38 +7,32 @@ import { SidebarCustom } from "../ui/components/SidebarCustom";
 import { Topbar } from "../ui/components/Topbar";
 
 export const AdminRoutes = () => {
-  return (
-    <>
-      <div className="app">
-        <SidebarCustom></SidebarCustom>
+	return (
+		<>
+			<div className="app">
+				<SidebarCustom></SidebarCustom>
 
-        <main className="content">
-          <Topbar></Topbar>
-          <Routes>
-            {/* EMPADRONAMIENTO*/}
-            <Route
-              path="/empadronamiento/*"
-              element={<EmpadronamientoRoutes />}
-            ></Route>
+				<main className="content">
+					<Topbar></Topbar>
+					<Routes>
+						{/* EMPADRONAMIENTO*/}
+						<Route
+							path="/empadronamiento/*"
+							element={<EmpadronamientoRoutes />}
+						></Route>
 
-            {/*JORNADA */}
-            <Route path="/jornada/*" element={<JornadaRoutes />}></Route>
+						{/*JORNADA */}
+						<Route path="/jornada/*" element={<JornadaRoutes />}></Route>
 
-            {/* PREPARACION */}
-            <Route
-              path="/preparacion/*"
-              element={<PreparacionRoutes />}
-            ></Route>
+						{/* PREPARACION */}
+						<Route path="/preparacion/*" element={<PreparacionRoutes />}></Route>
 
-            {/* RUTA POR DEFAULT */}
-            <Route
-              path="/"
-              element={<Navigate to="/preparacion/"></Navigate>}
-            ></Route>
-          </Routes>
-        </main>
-        {/* <div className="generalContainer">HOLA MUNDO</div> */}
-      </div>
-    </>
-  );
+						{/* RUTA POR DEFAULT */}
+						<Route path="/" element={<Navigate to="/preparacion/"></Navigate>}></Route>
+					</Routes>
+				</main>
+				{/* <div className="generalContainer">HOLA MUNDO</div> */}
+			</div>
+		</>
+	);
 };
