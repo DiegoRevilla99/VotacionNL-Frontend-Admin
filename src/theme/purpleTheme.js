7bdf200e489f6b78a7a6a7ae7f76b9e3c650f759
Diego Revilla <string.diego.revilla@gmail.com>
2022-12-02 23:38:04 -0600

    Quitando el thema de botones

--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -1,22 +1,20 @@
-import { createTheme } from '@mui/material/styles';
-
+import { createTheme } from "@mui/material/styles";
 
 export const purpleTheme = createTheme({
-
-    typography: {
-        button: {
-            textTransform: "none"
-        }
-    },
-    palette: {
-        primary: {
-            main: '#511079'
-        },
-        secondary: {
-            main: '#9E9E9E'
-        },
-        error: {
-            main: '#D80808'
-        }
-    }
-})
+	typography: {
+		// button: {
+		// 	textTransform: "none"
+		// },
+	},
+	palette: {
+		primary: {
+			main: "#511079",
+		},
+		secondary: {
+			main: "#9E9E9E",
+		},
+		error: {
+			main: "#D80808",
+		},
+	},
+});
