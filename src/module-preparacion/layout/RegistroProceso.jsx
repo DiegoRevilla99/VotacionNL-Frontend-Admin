4a8b3929f2647414ce2a45a3a480ed1646833c3e
Diego Revilla <string.diego.revilla@gmail.com>
2022-12-02 23:13:03 -0600

    Añadida plantilla para registro de proceso electpral

--- src/module-preparacion/layout/RegistroProceso.jsx
+++ src/module-preparacion/layout/RegistroProceso.jsx
@@ -1,16 +1,17 @@
 import { Box, Button, Divider, Grid, Typography } from "@mui/material";
-import React from "react";
+import { DataGridTable } from "../../ui/components/DataGridTable";
 
 export const RegistroProceso = ({ title }) => {
 	return (
 		<Grid
 			container
 			sx={{
+				height: "100%",
 				width: "100%",
 				overflowY: "auto",
 			}}
 		>
-			<Grid item xs={12}>
+			<Grid item xs={12} sx={{ display: "flex", flexDirection: "column" }}>
 				<Box sx={{ m: "0.5rem", ml: "2rem" }}>
 					<Typography variant="h6" align="left" color="initial">
 						REGISTRO DE JORNADA ELECTORAL
@@ -19,118 +20,66 @@ export const RegistroProceso = ({ title }) => {
 				<Divider />
 				<Box
 					sx={{
-						m: "4rem",
-						"& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							backgroundColor: "#C9A9CE !important",
-							boxShadow: 3,
+						height: "100%",
+						display: "flex",
+						flexDirection: "column",
+						m: "3rem",
+						mt: "2rem",
+						"& .css-1iyamrn-MuiButtonBase-root-MuiButton-root:hover": {
+							backgroundColor: "#7E328B !important",
+							transform: "translate(-5px, -5px)",
+							boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+							// boxShadow: 3,
 						},
 					}}
 				>
-					<Button
-						variant="contained"
-						size="large"
+					<Grid container>
+						<Grid item lg={3} md={4} sm={12} xs={12}>
+							<Button
+								variant="contained"
+								size="large"
+								sx={{
+									// boxShadow: 9,
+									boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+									transition: "all 0.5s ease",
+									backgroundColor: "#511079",
+									width: "100%",
+									borderTopLeftRadius: "0",
+									borderTopRightRadius: "1.6rem",
+									borderBottomLeftRadius: "1.6rem",
+									borderBottomRightRadius: "1.6rem",
+								}}
+							>
+								Registrar jornada electoral
+							</Button>
+						</Grid>
+					</Grid>
+
+					<Box
 						sx={{
-							backgroundColor: "#511079",
-							width: "12rem",
-							borderTopLeftRadius: "0",
-							borderTopRightRadius: "1.6rem",
-							borderBottomLeftRadius: "1.6rem",
-							borderBottomRightRadius: "1.6rem",
-							// "& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							// 	backgroundColor: "#511079 !important",
-							// },
-							// "& .MuiButton-root:hover": {
-							// 	backgroundColor: "#511079",
-							// },
-							// "& .MuiButton-contained:hover": {
-							// 	backgroundColor: "#511079",
-							// },
+							boxShadow: 1,
+							height: "100%",
+							display: "flex",
+							flexDirection: "column",
+							backgroundColor: "white",
+							mt: "2rem",
+							// height: "100%",
+							borderRadius: "2rem",
+							p: "2rem",
+							pt: "1rem",
 						}}
 					>
-						Registrar jornada electoral
-					</Button>
-				</Box>
-			</Grid>
-			<Grid item xs={12}>
-				<Box sx={{ m: "0.5rem", ml: "2rem" }}>
-					<Typography variant="h6" align="left" color="initial">
-						REGISTRO DE JORNADA ELECTORAL
-					</Typography>
-				</Box>
-				<Divider />
-				<Box
-					sx={{
-						m: "4rem",
-						"& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							backgroundColor: "#C9A9CE !important",
-							boxShadow: 3,
-						},
-					}}
-				>
-					<Button
-						variant="contained"
-						size="large"
-						sx={{
-							backgroundColor: "#511079",
-							width: "12rem",
-							borderTopLeftRadius: "0",
-							borderTopRightRadius: "1.6rem",
-							borderBottomLeftRadius: "1.6rem",
-							borderBottomRightRadius: "1.6rem",
-							// "& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							// 	backgroundColor: "#511079 !important",
-							// },
-							// "& .MuiButton-root:hover": {
-							// 	backgroundColor: "#511079",
-							// },
-							// "& .MuiButton-contained:hover": {
-							// 	backgroundColor: "#511079",
-							// },
-						}}
-					>
-						Registrar jornada electoral
-					</Button>
-				</Box>
-			</Grid>
-			<Grid item xs={12}>
-				<Box sx={{ m: "0.5rem", ml: "2rem" }}>
-					<Typography variant="h6" align="left" color="initial">
-						REGISTRO DE JORNADA ELECTORAL
-					</Typography>
-				</Box>
-				<Divider />
-				<Box
-					sx={{
-						m: "4rem",
-						"& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							backgroundColor: "#C9A9CE !important",
-							boxShadow: 3,
-						},
-					}}
-				>
-					<Button
-						variant="contained"
-						size="large"
-						sx={{
-							backgroundColor: "#511079",
-							width: "12rem",
-							borderTopLeftRadius: "0",
-							borderTopRightRadius: "1.6rem",
-							borderBottomLeftRadius: "1.6rem",
-							borderBottomRightRadius: "1.6rem",
-							// "& .css-hvnsit-MuiButtonBase-root-MuiButton-root:hover": {
-							// 	backgroundColor: "#511079 !important",
-							// },
-							// "& .MuiButton-root:hover": {
-							// 	backgroundColor: "#511079",
-							// },
-							// "& .MuiButton-contained:hover": {
-							// 	backgroundColor: "#511079",
-							// },
-						}}
-					>
-						Registrar jornada electoral
-					</Button>
+						<Typography variant="h5" color="initial" mb="0.5rem">
+							Jornadas Electorales
+						</Typography>
+						<Divider />
+						<Box
+							mt={"1rem"}
+							sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+						>
+							<DataGridTable />
+						</Box>
+					</Box>
 				</Box>
 			</Grid>
 		</Grid>
