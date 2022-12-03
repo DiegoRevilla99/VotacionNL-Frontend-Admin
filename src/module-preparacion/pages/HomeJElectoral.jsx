b69bc2e75d18dafa0bbbc2610e7d5113fd9f504f
kevin chavez <18161097@itoaxaca.edu.mx>
2022-12-02 18:41:38 -0600

    En este commit se ha agregado diseño a las dos primeras interfaces y mejorado el diseño a como se tenía

--- src/module-preparacion/pages/HomeJElectoral.jsx
+++ src/module-preparacion/pages/HomeJElectoral.jsx
@@ -10,34 +10,58 @@ import {
     TableCell, 
     TableRow } from '@mui/material';
 
-
 export const HomeJElectoral = () => {
   return (
     <>
     <div>
-        <Paper>
-          <Box p={1}>
-            <h2>JORNADA ELECTORAL</h2>
-            </Box>
-        </Paper>
+      <Box ml={3}>
+      <h3>JORNADA ELECTORAL</h3></Box>
+      <hr/>
     </div>
     <div>
-    <Box p={7}>
-        <Button variant="contained">REGISTRO DE LA JORNADA ELECTORAL</Button>
+    <Box ml={5} p={7}>
+        <Button sx={{   
+                   backgroundColor: '#511079',
+                   borderRadius: '0px 25px 25px 25px',
+                   color: '#fff',
+                   fontSize: '1.3rem',
+                   fontWeight: '700',
+                   letterSpacing: '0.01rem',
+                   lineHeight: '1.5rem',
+                   textAlign: 'center',
+                    boxShadow: '9px 10px 4px rgba(0, 0, 0, 0.37)',
+                    width: 220,
+                    height: 110,
+                    '&:hover': {
+                        background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+                    },}}>REGISTRO DE LA JORNADA ELECTORAL</Button>
     </Box>
     <Box p={2}>
         <TableContainer component = {Paper} sx={{maxHeight:'300px'}}>
             <Table aria-label = 'simple table' stickyHeader>
                 <TableHead>
                 <TableRow>
-                    <TableCell>JORNADAS ELECTORALES</TableCell>
+                    <TableCell><h3>JORNADAS ELECTORALES</h3></TableCell>
                     <TableCell></TableCell>
                 </TableRow>
                 </TableHead>
                 <TableBody>
                     <TableRow>
                         <TableCell>Elección Estatal 2022 Ordinaria</TableCell>
-                        <TableCell align='center'><Button variant="contained">CONFIGURACIÓN</Button></TableCell>
+                        <TableCell align='center'>
+                            <Button sx={{   
+                                backgroundColor: '#511079',
+                                borderRadius: 10,
+                                color: '#fff',
+                                boxShadow: 5,
+                                width: 170,
+                                height: 50,
+                                '&:hover': {
+                                    background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+                                },}}>
+                                CONFIGURACIÓN
+                            </Button>
+                        </TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
--- src/module-preparacion/pages/HomePreparacion.jsx
+++ src/module-preparacion/pages/HomePreparacion.jsx
@@ -1,29 +1,11 @@
 import React from "react";
-import { makeStyles } from '@mui/styles';
+
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-
-const useStyle = makeStyles({
-  boton: {
-    background: 'linear-gradient(45deg, #511079 30%, #511079 90%)',
-    borderRadius: '1px 19px 19px',
-    border: 1,
-    boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .23)",
-    color: "white",
-    padding: "0 30px",
-    width: 230,
-    height: 270,
-    '&:hover': {
-      background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
-  },
-  },
-});
-
 export const HomePreparacion = () => {
-  const classes = useStyle();
   return (
     <>
     <div>
@@ -34,30 +16,72 @@ export const HomePreparacion = () => {
 
     <div align = "center">
 
-      <Box mt={5}>
+      <Box mt={7}>
         <Box p={8} sx={{
           backgroundColor: '#FFFFFF',
           borderRadius: 10,
           border: 1,
           boxShadow: 5,
           width: 900,
-          height: 320,
+          height: 335,
           }}
           
           >
-            <Typography variant="h5" align="left" color="initial" paragraph>
+            <Typography ml={4} variant="h4" align="left" color="initial" paragraph>
               ¿Qué tipo de plantilla desea usar?
             </Typography>
         
         <Stack direction="row"
               justifyContent="center"
               alignItems="center"
-              spacing={10}
+              spacing={8}
               
               >  
-          <Button variant="contained" href="HomeJElectoral" className={classes.boton} >PLANTILLA DE LA JORNADA ELECTORAL</Button>
-          <Button variant="contained" className={classes.boton}>PLANTILLA DEL COMITÉ</Button>
-          <Button variant="contained" className={classes.boton}>PLANTILLA DE LA CONSULTA CIUDADANA</Button>
+          <Button href="HomeJElectoral" sx={{   
+                    backgroundColor: '#511079',
+                    borderRadius: '0px 25px 25px 25px',
+                    color: '#fff',
+                    boxShadow: '9px 10px 4px rgba(0, 0, 0, 0.37)',
+                    fontSize: '1.5rem',
+                    fontWeight: 'bold',
+                    letterSpacing: '0.01rem',
+                    lineHeight: '1.5rem',
+                    textAlign: 'center',
+                    width: 240,
+                    height: 270,
+                    '&:hover': {
+                        background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+                    },}} >PLANTILLA DE LA JORNADA ELECTORAL</Button>
+          <Button href="" sx={{   
+                    backgroundColor: '#511079',
+                    borderRadius: '0px 25px 25px 25px',
+                    color: '#fff',
+                    boxShadow: '9px 10px 4px rgba(0, 0, 0, 0.37)',
+                    fontSize: '1.5rem',
+                    fontWeight: 'bold',
+                    letterSpacing: '0.01rem',
+                    lineHeight: '1.5rem',
+                    textAlign: 'center',
+                    width: 240,
+                    height: 270,
+                    '&:hover': {
+                        background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+                    },}} >PLANTILLA DEL COMITÉ</Button>
+          <Button href="" sx={{   
+                    backgroundColor: '#511079',
+                    borderRadius: '0px 25px 25px 25px',
+                    color: '#fff',
+                    boxShadow: '9px 10px 4px rgba(0, 0, 0, 0.37)',
+                    fontSize: '1.5rem',
+                    fontWeight: 'bold',
+                    letterSpacing: '0.01rem',
+                    lineHeight: '1.5rem',
+                    textAlign: 'center',
+                    width: 240,
+                    height: 270,
+                    '&:hover': {
+                        background: 'linear-gradient(45deg, #C9A9CE 30%, #C9A9CE 90%)',
+                    },}} >PLANTILLA DE LA CONSULTA CIUDADANA</Button>
         </Stack>
       </Box>
       </Box>
