f82247bdd55b4f94a2c2a66691efcde67cbba852
Diego Revilla <string.diego.revilla@gmail.com>
2022-12-04 14:16:17 -0600

    Terminando modal para agregar preguntas a la consulta

--- src/module-preparacion/pages/AddPapeleta.jsx
+++ src/module-preparacion/pages/AddPapeleta.jsx
@@ -1,7 +1,13 @@
 import { Box, Divider, Grid, Typography, TextField, Paper, Button } from "@mui/material";
+import { useState } from "react";
 import { DataGridTable } from "../../ui/components/DataGridTable";
+import { ModalPapeleta } from "../components/ModalPapeleta";
 
 export const AddPapeleta = () => {
+	const [statusModal, setStatusModal] = useState(false);
+	const handleToggleModal = () => setStatusModal(!statusModal);
+	console.log(statusModal);
+
 	return (
 		<Box
 			sx={{
@@ -21,9 +27,6 @@ export const AddPapeleta = () => {
 				m={"2rem"}
 				sx={{
 					boxShadow: 1,
-					// height: "100%",
-					// display: "flex",
-					// flexDirection: "column",
 					backgroundColor: "white",
 					mt: "2rem",
 					borderRadius: "2rem",
@@ -128,7 +131,7 @@ export const AddPapeleta = () => {
 					</Grid>
 					<Grid item xs={12} md={6} lg={4}>
 						<Button
-							// onClick={add}
+							onClick={handleToggleModal}
 							variant="contained"
 							size="large"
 							sx={{
@@ -170,7 +173,61 @@ export const AddPapeleta = () => {
 						</Box>
 					</Grid>
 				</Grid>
+				<Grid mt={"1rem"} container direction="row" justifyContent="flex-end" spacing={2}>
+					<Grid item xs={12} md={6} lg={3}>
+						<Button
+							// onClick={handleToggleModal}
+							variant="contained"
+							size="large"
+							sx={{
+								// marginLeft: "5rem",
+								// marginRight: "5rem",
+								boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+								transition: "all 0.5s ease",
+								backgroundColor: "#511079",
+								width: "100%",
+								borderTopLeftRadius: "0",
+								borderTopRightRadius: "1.6rem",
+								borderBottomLeftRadius: "1.6rem",
+								borderBottomRightRadius: "1.6rem",
+								"&:hover": {
+									backgroundColor: "#7E328B !important",
+									transform: "translate(-5px, -5px)",
+									boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+								},
+							}}
+						>
+							Guardar
+						</Button>
+					</Grid>
+					<Grid item xs={12} md={6} lg={3}>
+						<Button
+							// onClick={handleToggleModal}
+							variant="contained"
+							size="large"
+							sx={{
+								boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+								transition: "all 0.5s ease",
+								backgroundColor: "#791010",
+								width: "100%",
+								borderTopLeftRadius: "0",
+
+								borderTopRightRadius: "1.6rem",
+								borderBottomLeftRadius: "1.6rem",
+								borderBottomRightRadius: "1.6rem",
+								"&:hover": {
+									backgroundColor: "#8B3232 !important",
+									transform: "translate(-5px, -5px)",
+									boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+								},
+							}}
+						>
+							Cancelar
+						</Button>
+					</Grid>
+				</Grid>
 			</Box>
+			<ModalPapeleta statusModal={statusModal} handleToggleModal={handleToggleModal} />
 		</Box>
 	);
 };
