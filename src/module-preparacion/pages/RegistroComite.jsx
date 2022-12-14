92fd58741e3ce27132e818602bc3449de8f37082
kevin chavez <18161097@itoaxaca.edu.mx>
2022-12-14 13:48:42 -0600

    Se agregaron los modales de eliminación, pero temporales

--- src/module-preparacion/pages/RegistroComite.jsx
+++ src/module-preparacion/pages/RegistroComite.jsx
@@ -1,12 +1,53 @@
 import { RegistroProceso } from "../layout/RegistroProceso";
 
+// CONECTAR EL MODAL DE ELIMINAR BOLETA
+// import {Grid, Button } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { ModalEliminarBoleta } from "../components/ModalEliminarBoleta";
+
 export const RegistroComite = () => {
+	// CONECTAR EL MODAL DE ELIMINAR BOLETA
+
+	// const navigate = useNavigate();
+	// const [statusDeleteModal, setStatusDeleteModal] = useState(false);
+	// const handleCloseDeleteModal = () => setStatusDeleteModal(false);
+	// const handleOpenDeleteModal = () => {
+	// 	// toastOffOperation();
+	// 	setStatusDeleteModal(true);
+	// };
+
 	return (
+		<>
 		<RegistroProceso
 			title={"REGISTRO DE COMITÉ"}
 			butonTitle={"Registrar Comité"}
 			tableTitle={"Comités"}
 			to={"/preparacion/comite"}
 		></RegistroProceso>
+			{/* <Grid item xs={4} md={2} lg={2}>
+			<Button
+			onClick={handleOpenDeleteModal}
+				variant="contained"
+				size="small"
+				disabled={status === "checking"}
+				sx={{
+					boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+					transition: "all 0.5s ease",
+					backgroundColor: "#791010",
+					width: "100%",
+					borderRadius: "25px 25px 25px 25px",
+					"&:hover": {
+						backgroundColor: "#8B3232 !important",
+						transform: "translate(-5px, -5px)",
+						boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+					},
+				}}
+			>
+				eliminar
+			</Button>
+		</Grid>
+		<ModalEliminarBoleta statusDeleteModal={statusDeleteModal} handleToggleModal={handleCloseDeleteModal} /> */}
+		</>
 	);
 };
