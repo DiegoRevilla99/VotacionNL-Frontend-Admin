7495489c9195d63c225c4bc8fe3fe9feed9c7c67
kevin chavez <18161097@itoaxaca.edu.mx>
2022-12-14 22:57:38 -0600

    Registro del votón modal para la eliminación

--- src/module-preparacion/pages/CrudComite.jsx
+++ src/module-preparacion/pages/CrudComite.jsx
@@ -11,6 +11,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useUiStore } from "../../hooks/useUiStore";
 import { saveComite } from "../../store/module-preparacion/comite/thunksComite";
 
+// CONECTAR EL MODAL DE ELIMINAR BOLETA
+// import {Grid, Button } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { ModalEliminarBoleta } from "../components/ModalEliminarBoleta";
+
 export const CrudComite = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -51,6 +57,15 @@ export const CrudComite = () => {
     },
   ];
 
+  	// CONECTAR EL MODAL DE ELIMINAR BOLETA
+
+	// const navigate = useNavigate();
+	// const [statusDeleteModal, setStatusDeleteModal] = useState(false);
+	// const handleCloseDeleteModal = () => setStatusDeleteModal(false);
+	// const handleOpenDeleteModal = () => {
+	// 	// toastOffOperation();
+	// 	setStatusDeleteModal(true);
+	// };
   return (
     <>
       <PlantillaCRUD
@@ -96,6 +111,29 @@ export const CrudComite = () => {
           </Box>
         </Box>
       </PlantillaCRUD>
+      {/* <Grid item xs={4} md={2} lg={2}>
+			<Button
+			onClick={handleOpenDeleteModal}
+				variant="contained"
+				size="small"
+				disabled={status === "checking"}
+				sx={{
+					boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+					transition: "all 0.5s ease",
+					backgroundColor: "#791010",
+					width: "100%",
+					borderRadius: "25px 25px 25px 25px",
+					"&:hover": {
+						backgroundColor: "#8B3232 !important",
+						transform: "translate(-5px, -5px)",
+						boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+					},
+				}}
+			>
+				eliminar
+			</Button>
+		</Grid>
+		<ModalEliminarBoleta statusDeleteModal={statusDeleteModal} handleToggleModal={handleCloseDeleteModal} /> */}
     </>
   );
 };
--- src/module-preparacion/pages/RegistroConsultaCiudadana.jsx
+++ src/module-preparacion/pages/RegistroConsultaCiudadana.jsx
@@ -1,20 +1,6 @@
 import { RegistroProceso } from "../layout/RegistroProceso";
 
-// CONECTAR EL MODAL DE ELIMINAR PAPELETA
-// import {Grid, Button } from "@mui/material";
-// import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
-// import { ModalEliminarPapeleta } from "../components/ModalEliminarPapeleta";
-
 export const RegistroConsultaCiudadana = () => {
-	// CONECTAR EL MODAL DE ELIMINAR PAPELETA
-	// const navigate = useNavigate();
-	// const [statusDeleteModal, setStatusDeleteModal] = useState(false);
-	// const handleCloseDeleteModal = () => setStatusDeleteModal(false);
-	// const handleOpenDeleteModal = () => {
-	// 	// toastOffOperation();
-	// 	setStatusDeleteModal(true);
-	// };
 
 	return (
 		<>
@@ -24,29 +10,6 @@ export const RegistroConsultaCiudadana = () => {
 			tableTitle={"Consultas Ciudadanas"}
 			to={"/preparacion/consulta"}
 		></RegistroProceso>
-		{/* <Grid item xs={4} md={2} lg={2}>
-			<Button
-			onClick={handleOpenDeleteModal}
-				variant="contained"
-				size="small"
-				disabled={status === "checking"}
-				sx={{
-					boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
-					transition: "all 0.5s ease",
-					backgroundColor: "#791010",
-					width: "100%",
-					borderRadius: "25px 25px 25px 25px",
-					"&:hover": {
-						backgroundColor: "#8B3232 !important",
-						transform: "translate(-5px, -5px)",
-						boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
-					},
-				}}
-			>
-				eliminar
-			</Button>
-		</Grid>
-		<ModalEliminarPapeleta statusDeleteModal={statusDeleteModal} handleToggleModal={handleCloseDeleteModal} /> */}
 		</>
 	);
 };
