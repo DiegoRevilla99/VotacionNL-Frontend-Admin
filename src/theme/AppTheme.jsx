b2ff87225810615ae7d2c65bbb4fc9f73ada3e29
Diego Revilla <string.diego.revilla@gmail.com>
2022-12-27 23:24:42 -0600

    Cambios finales

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { purpleTheme } from "./purpleTheme";
 
 export const AppTheme = ({ children }) => {
-  return (
-    <ThemeProvider theme={purpleTheme}>
-      <CssBaseline></CssBaseline>
-      {children}
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={purpleTheme}>
+			<CssBaseline></CssBaseline>
+			{children}
+		</ThemeProvider>
+	);
 };
