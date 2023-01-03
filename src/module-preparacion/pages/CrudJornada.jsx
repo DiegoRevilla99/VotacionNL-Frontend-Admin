07b1e137f365fc2c89ac39df27a22a226a1ba8b4
laurayess <56653712+laurayess@users.noreply.github.com>
2023-01-02 21:54:51 -0600

    Juntando flujos

--- src/module-preparacion/pages/CrudJornada.jsx
+++ src/module-preparacion/pages/CrudJornada.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, Divider, Grid, IconButton, LinearProgress, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { GeneralTable } from "../components/GeneralTable";
@@ -8,181 +16,192 @@ import BallotIcon from "@mui/icons-material/Ballot";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { useDispatch } from "react-redux";
 import {
-	onDeleteBoleta,
-	onGetBoletaData,
-	onGetBoletas,
-	onGetjornadas,
+  onDeleteBoleta,
+  onGetBoletaData,
+  onGetBoletas,
+  onGetjornadas,
 } from "../../store/module-preparacion/jornada/ThunksJornada";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useParams } from "react-router-dom";
 
 export const CrudJornada = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	// ToDo:AQUI OBTENGAN LAS VARIABLES STATUS Y DATA DE SUS ESTADOS GLOBALES
-	const { jornadaSelected, status } = useJornadaStore();
-	const params = useParams();
-	const dispatch = useDispatch();
-	const columns = [
-		// field: Debe de ir la variable que se va a mostrar en la tabla
-		{ field: "encabezadoBoleta", headerName: "Título de la boleta formal", flex: 10 },
-		{
-			field: "configuracion",
-			headerName: "Configuración",
-			flex: 5,
-			sortable: false,
-			disableColumnMenu: true,
-			renderCell: (params) => {
-				return (
-					<Stack spacing={2} direction="row">
-						<Button
-							variant="outlined"
-							startIcon={<BallotIcon />}
-							onClick={() => handleEdit(params.id)}
-						>
-							Ver
-						</Button>
-						<Button
-							variant="outlined"
-							startIcon={<SettingsIcon />}
-							onClick={() => handleConfig(params.id)}
-						>
-							Configuración
-						</Button>
-						<IconButton
-							sx={{ color: "#511079" }}
-							onClick={() => handleDelete(params.id)}
-						>
-							<DeleteIcon />
-						</IconButton>
-					</Stack>
-				);
-			},
-		},
-	];
+  // ToDo:AQUI OBTENGAN LAS VARIABLES STATUS Y DATA DE SUS ESTADOS GLOBALES
+  const { jornadaSelected, status } = useJornadaStore();
+  const params = useParams();
+  const dispatch = useDispatch();
+  const columns = [
+    // field: Debe de ir la variable que se va a mostrar en la tabla
+    {
+      field: "encabezadoBoleta",
+      headerName: "Título de la boleta formal",
+      flex: 10,
+    },
+    {
+      field: "configuracion",
+      headerName: "Configuración",
+      flex: 5,
+      sortable: false,
+      disableColumnMenu: true,
+      renderCell: (params) => {
+        return (
+          <Stack spacing={2} direction="row">
+            <Button
+              variant="outlined"
+              startIcon={<BallotIcon />}
+              onClick={() => handleEdit(params.id)}
+            >
+              Ver
+            </Button>
+            <Button
+              variant="outlined"
+              startIcon={<SettingsIcon />}
+              onClick={() => handleConfig(params.id)}
+            >
+              Configuración
+            </Button>
+            <IconButton
+              sx={{ color: "#511079" }}
+              onClick={() => handleDelete(params.id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Stack>
+        );
+      },
+    },
+  ];
 
-	// USEEFFECT QUE PUEDES USAR PARA HACER UN GET DE LAS JORNADAS AL RENDERIZAR LA PAGINA
-	useEffect(() => {
-		dispatch(onGetBoletas(params.id));
-	}, []);
+  // USEEFFECT QUE PUEDES USAR PARA HACER UN GET DE LAS JORNADAS AL RENDERIZAR LA PAGINA
+  useEffect(() => {
+    dispatch(onGetBoletas(params.id));
+  }, []);
 
-	// METODO PARA BORRAR UN REGISTRO
-	const handleDelete = (id) => {
-		dispatch(onDeleteBoleta(id));
-	};
+  // METODO PARA BORRAR UN REGISTRO
+  const handleDelete = (id) => {
+    dispatch(onDeleteBoleta(id));
+  };
 
-	// MÉTODO PARA EDITAR UN REGISTRO
-	const handleEdit = (id) => {
-		dispatch(
-			onGetBoletaData(id, () => {
-				navigate("/preparacion/jornada/" + params.id + "/boleta/" + id);
-			})
-		);
-	};
+  // MÉTODO PARA EDITAR UN REGISTRO
+  const handleEdit = (id) => {
+    dispatch(
+      onGetBoletaData(id, () => {
+        navigate("/preparacion/jornada/" + params.id + "/boleta/" + id);
+      })
+    );
+  };
 
-	// MÉTODO PARA IR A LA PAGINA DE CONFIGURACIÓN DEL REGISTRO
-	const handleConfig = (id) => {
-		// navigate("/preparacion/consulta/config/" + id);
-		// dispatch(onGetConfig(id));
-	};
+  // MÉTODO PARA IR A LA PAGINA DE CONFIGURACIÓN DEL REGISTRO
+  const handleConfig = (id) => {
+    navigate("/preparacion/jornada/configboleta/" + id);
+    // dispatch(onGetConfig(id));
+  };
 
-	const handleAdd = () => {
-		// navigate("/preparacion/jornada/boleta/");
-		navigate(
-			"/preparacion/jornada/" + params.id + "/boleta/" + jornadaSelected.boletas.length
-		);
-	};
+  const handleAdd = () => {
+    // navigate("/preparacion/jornada/boleta/");
+    navigate(
+      "/preparacion/jornada/" +
+        params.id +
+        "/boleta/" +
+        jornadaSelected.boletas.length
+    );
+  };
 
-	if (status === "checking")
-		return (
-			<Box sx={{ width: "100%" }}>
-				<LinearProgress />
-			</Box>
-		);
-	else
-	return (
-			<Grid
-				container
-				sx={{
-					height: "100%",
-					width: "100%",
-					overflowY: "auto",
-				}}
-			>
-				<Grid item xs={12} sx={{ display: "flex", flexDirection: "column" }}>
-					<Box sx={{ m: "0.5rem", ml: "2rem" }}>
-						<Typography variant="h6" align="left" color="initial">
-							{jornadaSelected.title}
-						</Typography>
-					</Box>
-					<Divider />
-					<Box
-						sx={{
-							height: "100%",
-							display: "flex",
-							flexDirection: "column",
-							m: "2rem",
-							mt: "2rem",
-						}}
-					>
-						<Grid container>
-							<Grid item lg={3} md={4} sm={12} xs={12}>
-								<Button
-									onClick={handleAdd}
-									variant="contained"
-									size="large"
-									sx={{
-										boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
-										transition: "all 0.5s ease",
-										backgroundColor: "#511079",
-										width: "100%",
-										borderRadius: "2rem 2rem 2rem 2rem",
-										"&:hover": {
-											backgroundColor: "#7E328B !important",
-											transform: "translate(-5px, -5px)",
-											boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
-										},
-									}}
-								>
-									Registrar Boleta Formal
-								</Button>
-							</Grid>
-						</Grid>
+  if (status === "checking")
+    return (
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    );
+  else
+    return (
+      <Grid
+        container
+        sx={{
+          height: "100%",
+          width: "100%",
+          overflowY: "auto",
+        }}
+      >
+        <Grid item xs={12} sx={{ display: "flex", flexDirection: "column" }}>
+          <Box sx={{ m: "0.5rem", ml: "2rem" }}>
+            <Typography variant="h6" align="left" color="initial">
+              {jornadaSelected.title}
+            </Typography>
+          </Box>
+          <Divider />
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              m: "2rem",
+              mt: "2rem",
+            }}
+          >
+            <Grid container>
+              <Grid item lg={3} md={4} sm={12} xs={12}>
+                <Button
+                  onClick={handleAdd}
+                  variant="contained"
+                  size="large"
+                  sx={{
+                    boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.3)",
+                    transition: "all 0.5s ease",
+                    backgroundColor: "#511079",
+                    width: "100%",
+                    borderRadius: "2rem 2rem 2rem 2rem",
+                    "&:hover": {
+                      backgroundColor: "#7E328B !important",
+                      transform: "translate(-5px, -5px)",
+                      boxShadow: "5px 5px 1px rgba(0, 0, 0, 0.3)",
+                    },
+                  }}
+                >
+                  Registrar Boleta Formal
+                </Button>
+              </Grid>
+            </Grid>
 
-						<Box
-							sx={{
-								boxShadow: 1,
-								height: "100%",
-								display: "flex",
-								flexDirection: "column",
-								backgroundColor: "white",
-								mt: "2rem",
-								borderRadius: "2rem",
-								p: "2rem",
-								pt: "1rem",
-							}}
-						>
-							<Typography variant="h5" color="initial" mb="0.5rem">
-								Boletas Formales
-							</Typography>
-							<Divider />
-							<Box
-								mt={"1rem"}
-								sx={{ height: "100%", display: "flex", flexDirection: "column" }}
-							>
-								{/* TABLA GENERAL, TIENEN QUE PASARLE LA DATA DE LOS REGISTROS Y EL ID DE 
+            <Box
+              sx={{
+                boxShadow: 1,
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "white",
+                mt: "2rem",
+                borderRadius: "2rem",
+                p: "2rem",
+                pt: "1rem",
+              }}
+            >
+              <Typography variant="h5" color="initial" mb="0.5rem">
+                Boletas Formales
+              </Typography>
+              <Divider />
+              <Box
+                mt={"1rem"}
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                {/* TABLA GENERAL, TIENEN QUE PASARLE LA DATA DE LOS REGISTROS Y EL ID DE 
                                 CADA REGISTRO SE DEBE LLAMAR "idJornada" o si el id de cada registro 
                                 tiene otro nombre, cambien el atributo idName al nombre que quieran */}
-								<GeneralTable
-									data={jornadaSelected.boletas} // DATA DE LOS REGISTROS
-									columns={columns}
-									idName={"idEstructuraBoleta"} // NOMBRE DEL ID DE CADA REGISTRO
-								/>
-							</Box>
-						</Box>
-					</Box>
-				</Grid>
-			</Grid>
-		);
+                <GeneralTable
+                  data={jornadaSelected.boletas} // DATA DE LOS REGISTROS
+                  columns={columns}
+                  idName={"idEstructuraBoleta"} // NOMBRE DEL ID DE CADA REGISTRO
+                />
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    );
 };
