a579a77badee916fb7e96e081b15bc556aa406b1
Diego Revilla <string.diego.revilla@gmail.com>
2023-01-19 18:25:45 -0600

    Iniciando modulo jornada

--- src/ui/components/SidebarCustom.jsx
+++ src/ui/components/SidebarCustom.jsx
@@ -13,144 +13,142 @@ import { Item } from "./Item";
 import { DividerItem } from "./DividerItem";
 
 export const SidebarCustom = () => {
-  const [isCollapsed, setisCollapsed] = useState(false);
-  const [selected, setSelected] = useState("Inicio");
-  console.log(selected);
+	const [isCollapsed, setisCollapsed] = useState(false);
+	const [selected, setSelected] = useState("Inicio");
+	console.log(selected);
 
-  const getSize = () => {
-    const width = window.innerWidth;
-    if (width < 1200 && !isCollapsed) {
-      console.log("COLAPSA");
-      setisCollapsed(true);
-    } else if (width > 1200 && isCollapsed) {
-      console.log("DESCOLAPSA");
-      setisCollapsed(false);
-    }
-  };
+	const getSize = () => {
+		const width = window.innerWidth;
+		if (width < 1200 && !isCollapsed) {
+			console.log("COLAPSA");
+			setisCollapsed(true);
+		} else if (width > 1200 && isCollapsed) {
+			console.log("DESCOLAPSA");
+			setisCollapsed(false);
+		}
+	};
 
-  useLayoutEffect(() => {
-    window.addEventListener("resize", getSize);
-    return () => {
-      window.removeEventListener("resize", getSize);
-    };
-  }, [window.innerWidth]);
+	useLayoutEffect(() => {
+		window.addEventListener("resize", getSize);
+		return () => {
+			window.removeEventListener("resize", getSize);
+		};
+	}, [window.innerWidth]);
 
-  return (
-    <Box
-      sx={{
-        "& .pro-sidebar-inner": {
-          background: "#7E328B !important",
-        },
-        "& .pro-icon-wrapper": {
-          backgroundColor: "transparent !important",
-        },
-        "& .pro-menu": {
-          paddingTop: "0px !important",
-        },
-        "& .pro-inner-item": {
-          padding: "5px 35px 5px 20px !important",
-        },
-        "& .pro-inner-item:hover": {
-          color: "currentColor !important",
-        },
-        "& .pro-menu-item.active": {
-          color: "#7E328B !important",
-        },
-        "& .pro-inner-item:focus": {
-          color: "#7E328B !important",
-        },
-      }}
-    >
-      <ProSidebar collapsed={isCollapsed} breakPoint="md">
-        <Box mt="0px" pt="0px">
-          <Menu iconShape="square">
-            {/* USUARIO */}
-            <Box
-              mb="25px"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              sx={{
-                background: "#f8f7f3",
-                height: "73.99px",
-                boxShadow: 3,
-              }}
-            >
-              <Box>
-                <img
-                  alt="logo"
-                  src="../../CEE600x321.png"
-                  style={{
-                    transition: "width 0.5s, height 0.5s",
-                    width: isCollapsed ? "75px" : "139px",
-                    height: isCollapsed ? "40px" : "73.99px",
-                  }}
-                />
-              </Box>
-            </Box>
+	return (
+		<Box
+			sx={{
+				"& .pro-sidebar-inner": {
+					background: "#7E328B !important",
+				},
+				"& .pro-icon-wrapper": {
+					backgroundColor: "transparent !important",
+				},
+				"& .pro-menu": {
+					paddingTop: "0px !important",
+				},
+				"& .pro-inner-item": {
+					padding: "5px 35px 5px 20px !important",
+				},
+				"& .pro-inner-item:hover": {
+					color: "currentColor !important",
+				},
+				"& .pro-menu-item.active": {
+					color: "#7E328B !important",
+				},
+				"& .pro-inner-item:focus": {
+					color: "#7E328B !important",
+				},
+			}}
+		>
+			<ProSidebar collapsed={isCollapsed} breakPoint="md">
+				<Box mt="0px" pt="0px">
+					<Menu iconShape="square">
+						{/* USUARIO */}
+						<Box
+							mb="25px"
+							display="flex"
+							justifyContent="center"
+							alignItems="center"
+							sx={{
+								background: "#f8f7f3",
+								height: "73.99px",
+								boxShadow: 3,
+							}}
+						>
+							<Box>
+								<img
+									alt="logo"
+									src="../../CEE600x321.png"
+									style={{
+										transition: "width 0.5s, height 0.5s",
+										width: isCollapsed ? "75px" : "139px",
+										height: isCollapsed ? "40px" : "73.99px",
+									}}
+								/>
+							</Box>
+						</Box>
 
-            {/* MENU ITEMS */}
-            <Box paddingLeft={isCollapsed ? undefined : "5%"} marginTop="1rem">
-              <DividerItem
-                isCollapsed={isCollapsed}
-                title={"Principal"}
-              ></DividerItem>
+						{/* MENU ITEMS */}
+						<Box paddingLeft={isCollapsed ? undefined : "5%"} marginTop="1rem">
+							<DividerItem
+								isCollapsed={isCollapsed}
+								title={"Principal"}
+							></DividerItem>
 
-              <Item
-                title="Inicio"
-                icon={<HomeOutLinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              ></Item>
-              <Item
-                title="Preparación"
-                icon={<EditOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-                to={"preparacion/inicio"}
-              ></Item>
-              <Item
-                title="Empadronamiento"
-                icon={<PersonAddAltOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-                to={"empadronamiento/inicio/formal"}
-              ></Item>
-              <Item
-                title="Jornada"
-                icon={<BarChartOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              ></Item>
-              <DividerItem
-                isCollapsed={isCollapsed}
-                title={"Otros"}
-              ></DividerItem>
-              <Item
-                title="Configuración"
-                icon={<SettingsOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              ></Item>
-            </Box>
+							<Item
+								title="Inicio"
+								icon={<HomeOutLinedIcon />}
+								selected={selected}
+								setSelected={setSelected}
+							></Item>
+							<Item
+								title="Preparación"
+								icon={<EditOutlinedIcon />}
+								selected={selected}
+								setSelected={setSelected}
+								to={"preparacion/inicio"}
+							></Item>
+							<Item
+								title="Empadronamiento"
+								icon={<PersonAddAltOutlinedIcon />}
+								selected={selected}
+								setSelected={setSelected}
+								to={"empadronamiento/inicio/formal"}
+							></Item>
+							<Item
+								title="Jornada"
+								icon={<BarChartOutlinedIcon />}
+								selected={selected}
+								setSelected={setSelected}
+								to={"jornada/inicio"}
+							></Item>
+							<DividerItem isCollapsed={isCollapsed} title={"Otros"}></DividerItem>
+							<Item
+								title="Configuración"
+								icon={<SettingsOutlinedIcon />}
+								selected={selected}
+								setSelected={setSelected}
+							></Item>
+						</Box>
 
-            <Box
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              paddingRight="5px"
-              sx={{ color: "white", paddingTop: "5rem" }}
-            >
-              <IconButton
-                onClick={() => setisCollapsed(!isCollapsed)}
-                sx={{ color: "white" }}
-              >
-                <MenuOutlinedIcon />
-              </IconButton>
-            </Box>
-          </Menu>
-        </Box>
-      </ProSidebar>
-    </Box>
-  );
+						<Box
+							display="flex"
+							justifyContent="center"
+							alignItems="center"
+							paddingRight="5px"
+							sx={{ color: "white", paddingTop: "5rem" }}
+						>
+							<IconButton
+								onClick={() => setisCollapsed(!isCollapsed)}
+								sx={{ color: "white" }}
+							>
+								<MenuOutlinedIcon />
+							</IconButton>
+						</Box>
+					</Menu>
+				</Box>
+			</ProSidebar>
+		</Box>
+	);
 };
